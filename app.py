8f33c17218a5a38371ce457670f51affeccc5d38
jerry wu <a0938489128@gmail.com>
2020-11-03 12:59:09 +0800

    fix file

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask
 from redis import Redis
 app= Flask(__name__)
-
 redis = Redis (host="redis")
 
 @app.route("/")
-def hello ():
+def hello():
     visits = redis.incr('counter')
-    html = "<h3>Hello World!</h3>"
-           "<b>Visits:</b> {visits}"
+    html = "<h3>Hello World!</h3>" \
+           "<b>Visits:</b> {visits}" \
            "<br/>"
     return html.format(visits=visits)
 if __name__  =="__main__":
-    app.run (host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80)
